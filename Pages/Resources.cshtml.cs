cff18ed91e0a00f3b2ad54d4968037c84644f2ce
Shivam <N0949022@ntu.ac.uk>
2024-07-23 20:25:15 +0100

    Created base for resources page.

---
+++ Pages/Resources.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoodTracker.Pages
+{
+    public class ResourcesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
